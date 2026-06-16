42c40ae8d9d599f261bb70d986b85d90954dcd07
7h1n.9 <3459674645@qq.com>
2026-06-16 09:56:57 +0800

    first commit

---
+++ ecommerce-backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+# app/models/__init__.py
+from app.models.user import User
+from app.models.product import Product
+from app.models.cart import CartItem
+from app.models.order import Order, OrderItem
+
+__all__ = ["User", "Product", "CartItem", "Order", "OrderItem"]
